a78e787b1f15a685d02b6f80c418242083e155e9
luciabalbas <luciabalrub@gmail.com>
2025-09-03 19:50:37 +0200

    Creating Signup Page

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // style
 import style from './Signup.module.css'
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password, username)
+    setEmail('')
+    setPassword('')
+    setUsername('')
+  }
+
   return (
-    <div>
-      Signup
-    </div>
+    <form className={style['signup-form']} onSubmit={handleSubmit}>
+      <h2>Signup</h2>
+      <label>
+        <span>email:</span>
+        <input 
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input 
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>Username:</span>
+        <input 
+          type='text'
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+      </label>
+      <button className='btn'>Signup</button>
+    </form>
   )
 }
