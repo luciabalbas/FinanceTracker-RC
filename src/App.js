112bc57cb5c16e2f39060833682003801ca603e0
luciabalbas <luciabalrub@gmail.com>
2025-09-03 14:51:47 +0200

    Creating Routes to the website pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+
+// import Pages
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={Signup} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
