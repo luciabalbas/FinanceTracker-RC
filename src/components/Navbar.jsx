017c82dcd841e4d6925e98d02d5ba129d3404ff3
luciabalbas <luciabalrub@gmail.com>
2025-09-04 12:19:25 +0200

    Modifying Navbar Component to Conditional User Content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // style
 import style from './Navbar.module.css'
 
 export default function Navbar() {
   const { logout } = useLogout()
+  const { user } = useAuthContext()
+
 
   return (
     <nav className={style.navbar}>
         <ul>
             <li className={style.title}>myMoney</li>
-            <li><Link to='/login'>Login</Link></li>
-            <li><Link to='/signup'>Signup</Link></li>
-            <li><button className="btn" onClick={logout}>Logout</button></li>
+            {!user && (
+              <>
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/signup'>Signup</Link></li>
+              </>
+            )}
+            {user && (
+              <>
+                <li>hello, {user.displayName}</li>
+                <li>
+                  <button className="btn" onClick={logout}>Logout</button>
+                </li>
+              </>
+            )}
         </ul>
     </nav>
   )
