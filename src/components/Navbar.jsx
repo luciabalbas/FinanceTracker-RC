0b680412759318d3341887c9a07bc5b17b839168
luciabalbas <luciabalrub@gmail.com>
2025-09-04 10:56:56 +0200

    Implementing useLogout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { useLogout } from '../hooks/useLogout'
 
 // style
 import style from './Navbar.module.css'
 
 export default function Navbar() {
+  const { logout } = useLogout()
+
   return (
     <nav className={style.navbar}>
         <ul>
             <li className={style.title}>myMoney</li>
             <li><Link to='/login'>Login</Link></li>
             <li><Link to='/signup'>Signup</Link></li>
+            <li><button className="btn" onClick={logout}>Logout</button></li>
         </ul>
     </nav>
   )
