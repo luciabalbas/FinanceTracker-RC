72387b7987c948d362fa053b78bef5b64d48df71
luciabalbas <luciabalrub@gmail.com>
2025-09-05 12:31:09 +0200

    Creating the Transaction Form

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
+// components
+import TransactionForm from './TransationForm'
+
 //style
 import styles from './Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.content}>
+        transaction list
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
     </div>
   )
 }
