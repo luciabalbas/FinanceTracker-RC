dbcc13ea6d0c0295ca774bfcc86c40c7630f53bd
luciabalbas <luciabalrub@gmail.com>
2025-09-05 14:37:12 +0200

    Updating TransitionForm for using useFirestore

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 // components
 import TransactionForm from './TransationForm'
@@ -7,13 +8,15 @@ import TransactionForm from './TransationForm'
 import styles from './Home.module.css'
 
 export default function Home() {
+  const { user } = useAuthContext()
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         transaction list
       </div>
       <div className={styles.sidebar}>
-        <TransactionForm />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
   )
