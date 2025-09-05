a0390907be4ff6c159b33fe3028be2b90412be30
luciabalbas <luciabalrub@gmail.com>
2025-09-05 14:20:49 +0200

    Updating useFirestore to add new documents

--- src/hooks/useFirestore.jsx
+++ src/hooks/useFirestore.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from 'react'
+import React, { act, useEffect, useReducer, useState } from 'react'
 import { projectFirestore } from '../firebase/config'
 
 const initialState = {
@@ -10,6 +10,27 @@ const initialState = {
 
 const firestoreReducer = (state, action) => {
     switch (action.type) {
+        case 'IS_PENDING':
+            return {
+                document: null,
+                isPending: true,
+                error: null,
+                success: false
+            }
+        case 'ADDED_DOCUMENT':
+            return {
+                document: action.payload,
+                isPending: false,
+                error: null,
+                success: true
+            }
+        case 'ERROR':
+            return {
+                document: null,
+                isPending: false,
+                error: action.payload,
+                success: false
+            }
         default:
             return state
     }
@@ -22,13 +43,26 @@ export const useFirestore = (collection) => {
     // collection ref
     const ref = projectFirestore.collection(collection)
 
-    // add a document
-    const addDocument = (doc) => {
+    // dispatch if not cancelled
+    const dispatchIFNotCancelled = (action) => {
+        if (!isCancelled) {
+          dispatch(action)  
+        } 
+    }
 
+    // add a document
+    const addDocument = async (doc) => {
+        dispatch({ type: 'IS_PENDING' })
+        try {
+            const addedDocument = await ref.add(doc)
+            dispatchIFNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument })
+        } catch (error) {
+            dispatchIFNotCancelled({ type: 'ERROR', payload: error.message })
+        }
     }
 
     // delete a document
-    const deleteDocument = (id) => {
+    const deleteDocument = async (id) => {
 
     }
 
